7f5029033711cfb694d79cd067605f29a7dbbd03
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-07-20 15:30:36 +0900

    new file: PSChromeCli.psm1

---
+++ PSChromeCli.psm1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+Chrome がインストールされているフォルダーのパスを返します。
+#>
+function Get-ChromePath(){
+    #C:\Program Files (x86)\Google\Chrome\Application
+    if(Test-Path "${env:ProgramFiles}/Google/Chrome/Application"){
+        $ChromePath=Resolve-Path "${env:ProgramFiles}/Google/Chrome/Application"
+    }elseif(Test-Path "${env:ProgramFiles(x86)}/Google/Chrome/Application"){
+        $ChromePath=Resolve-Path "${env:ProgramFiles(x86)}/Google/Chrome/Application"
+    }
+
+    return $ChromePath
+}
+
+<#
+.SYNOPSIS
+環境変数 Path へ Chrome がインストールされているフォルダーのパスを追加します。
+#>
+function Set-ChromePath(){
+    $ChromePath=Get-ChromePath
+    $env:Path="${env:Path}${ChromePath};"
+}
